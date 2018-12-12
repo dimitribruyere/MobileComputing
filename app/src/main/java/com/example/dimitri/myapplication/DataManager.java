def87ac99757117634478ec945cb8b24a2d5c056
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-12-12 14:26:15 +0100

    Utilisation du DataManager pour transmettre le message d'une activité à l'autre

--- app/src/main/java/com/example/dimitri/myapplication/DataManager.java
+++ app/src/main/java/com/example/dimitri/myapplication/DataManager.java
@@ -3,6 +3,7 @@ package com.example.dimitri.myapplication;
 public class DataManager {
 
     private static DataManager INSTANCE;
+    public String name;
 
     private DataManager(){
     }
@@ -14,4 +15,15 @@ public class DataManager {
         return INSTANCE;
     }
 
+    public static void setInstance(DataManager INSTANCE) {
+        DataManager.INSTANCE = INSTANCE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
